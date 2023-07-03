3264db07501986ebf5274df63fd3b688ab03299c
ValdirCezar <valdircezar312@gmail.com>
2023-07-03 11:07:06 -0300

    feat: add user entity

---
+++ user-service-api/src/main/java/br/com/valdircezar/userserviceapi/entity/User.java
@@ -0,0 +1,26 @@
+package br.com.valdircezar.userserviceapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import models.enums.ProfileEnum;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+@With
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<ProfileEnum> profile;
+}
