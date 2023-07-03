5cbdafbb20c8e80039a3ea33ad39581816edec95
ValdirCezar <valdircezar312@gmail.com>
2023-07-03 10:15:46 -0300

    chore: start user-service-api

---
+++ user-service-api/src/main/java/br/com/valdircezar/userserviceapi/UserServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.userserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApiApplication.class, args);
+    }
+
+}
---
+++ user-service-api/src/test/java/br/com/valdircezar/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
