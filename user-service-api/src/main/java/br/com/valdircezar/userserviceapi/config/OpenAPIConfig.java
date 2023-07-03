0ebaad0f1ca6d22be970a5814c01d62ab2f2eb9a
ValdirCezar <valdircezar312@gmail.com>
2023-07-03 14:49:17 -0300

    docs: add openapi docs

---
+++ user-service-api/src/main/java/br/com/valdircezar/userserviceapi/config/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package br.com.valdircezar.userserviceapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(
+            @Value("${springdoc.openapi.title}") final String title,
+            @Value("${springdoc.openapi.description}") final String description,
+            @Value("${springdoc.openapi.version}") final String version
+    ) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .description(description)
+                        .version(version)
+                );
+    }
+}
--- user-service-api/src/main/java/br/com/valdircezar/userserviceapi/controller/UserController.java
+++ user-service-api/src/main/java/br/com/valdircezar/userserviceapi/controller/UserController.java
@@ -1,14 +1,42 @@
 package br.com.valdircezar.userserviceapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import models.exceptions.StandardError;
 import models.responses.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "UserController", description = "Controller responsible for user operations")
 @RequestMapping("/api/users")
 public interface UserController {
 
+    @Operation(summary = "Find user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(
+                    responseCode = "404", description = "User not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StandardError.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "500", description = "Internal server error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StandardError.class)
+                    ))
+    })
     @GetMapping("/{id}")
-    ResponseEntity<UserResponse> findById(@PathVariable(name = "id") final String id);
+    ResponseEntity<UserResponse> findById(
+            @Parameter(description = "User id", required = true, example = "64a2dc9d48a6a977cdca11c8")
+            @PathVariable(name = "id") final String id
+    );
 }
