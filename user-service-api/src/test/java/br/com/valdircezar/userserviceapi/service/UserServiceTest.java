59b220b511009e095febf7b5173056752252ef77
ValdirCezar <valdircezar312@gmail.com>
2023-07-05 16:08:55 -0300

    test: add test to find by id with success user service

---
+++ user-service-api/src/test/java/br/com/valdircezar/userserviceapi/service/UserServiceTest.java
@@ -0,0 +1,50 @@
+package br.com.valdircezar.userserviceapi.service;
+
+import br.com.valdircezar.userserviceapi.entity.User;
+import br.com.valdircezar.userserviceapi.mapper.UserMapper;
+import br.com.valdircezar.userserviceapi.repository.UserRepository;
+import models.responses.UserResponse;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService service;
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @Mock
+    private BCryptPasswordEncoder encoder;
+
+    @Test
+    void whenCallFindByIdWithValidIdThenReturnUserResponse() {
+        when(repository.findById(anyString())).thenReturn(Optional.of(new User()));
+        when(mapper.fromEntity(any(User.class))).thenReturn(mock(UserResponse.class));
+
+        final var response = service.findById("1");
+
+        assertNotNull(response);
+        assertEquals(UserResponse.class, response.getClass());
+
+        verify(repository, times(1)).findById(anyString());
+        verify(mapper, times(1)).fromEntity(any(User.class));
+    }
+
+}
