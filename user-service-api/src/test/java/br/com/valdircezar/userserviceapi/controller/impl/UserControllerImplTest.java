3f3def645b4d4ce373d8e31cf1df7fa75e7ca2c0
ValdirCezar <valdircezar312@gmail.com>
2023-07-06 14:40:20 -0300

    test: add integration test to find by id user controller

---
+++ user-service-api/src/test/java/br/com/valdircezar/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -0,0 +1,43 @@
+package br.com.valdircezar.userserviceapi.controller.impl;
+
+import br.com.valdircezar.userserviceapi.entity.User;
+import br.com.valdircezar.userserviceapi.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static br.com.valdircezar.userserviceapi.creator.CreatorUtils.generateMock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserControllerImplTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testFindByIdWithSuccess() throws Exception {
+        final var entity = generateMock(User.class);
+        final var userId =  userRepository.save(entity).getId();
+
+        mockMvc.perform(get("/api/users/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.name").value(entity.getName()))
+                .andExpect(jsonPath("$.email").value(entity.getEmail()))
+                .andExpect(jsonPath("$.password").value(entity.getPassword()))
+                .andExpect(jsonPath("$.profiles").isArray());
+
+        userRepository.deleteById(userId);
+    }
+}
