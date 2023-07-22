608941a3065d873967ea5ac85107e285a536937c
ValdirCezar <valdircezar312@gmail.com>
2023-07-21 23:50:53 -0300

    chore: starter auth-service-api

---
+++ auth-service-api/src/main/java/br/com/valdircezar/authserviceapi/AuthServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.authserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApiApplication.class, args);
+    }
+
+}
---
+++ auth-service-api/src/test/java/br/com/valdircezar/authserviceapi/AuthServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.authserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
