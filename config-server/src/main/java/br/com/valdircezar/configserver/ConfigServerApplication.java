174a4b6b5757453802ebf1737ecba6f42215629e
valdircezar <valdircezar312@gmail.com>
2023-08-05 12:55:31 -0300

    chore: add github access

--- config-server/src/main/java/br/com/valdircezar/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/br/com/valdircezar/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package br.com.valdircezar.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
