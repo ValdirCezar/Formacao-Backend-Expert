54b1b7ab19fa85bef241e9dfc95b6091da73adf7
valdircezar <valdircezar312@gmail.com>
2023-08-05 12:34:08 -0300

    chore: start config server

---
+++ config-server/src/main/java/br/com/valdircezar/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
