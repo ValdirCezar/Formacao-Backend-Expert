c629be28398cfb87e80a5dc3941947c9c18d42a5
valdircezar <valdircezar312@gmail.com>
2023-08-19 09:57:36 -0300

    chore: start order service api

---
+++ order-service-api/src/main/java/br/com/valdircezar/orderserviceapi/OrderServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.orderserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApiApplication.class, args);
+    }
+
+}
