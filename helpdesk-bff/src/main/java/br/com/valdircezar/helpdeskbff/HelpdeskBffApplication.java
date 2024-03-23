6b90f249b21c6f0a7db7cf6ade2ad6800a225804
valdircezar <valdircezar312@gmail.com>
2024-03-23 07:42:04 -0300

    chore: start helpdesk bff project

---
+++ helpdesk-bff/src/main/java/br/com/valdircezar/helpdeskbff/HelpdeskBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdircezar.helpdeskbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpdeskBffApplication.class, args);
+    }
+
+}
