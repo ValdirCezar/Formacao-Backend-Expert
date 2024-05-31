cbb87d73c719310f7b74d80aacbd53ffaa91ac05
valdircezar <valdir_jesus@sicredi.com.br>
2024-05-31 15:35:53 -0300

    feat: add authorization filter p1

---
+++ helpdesk-bff/src/main/java/br/com/valdircezar/helpdeskbff/security/JWTAuthorizationFilter.java
@@ -0,0 +1,53 @@
+package br.com.valdircezar.helpdeskbff.security;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        final String authHeader = request.getHeader(AUTHORIZATION);
+
+        if (authHeader == null) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        if (authHeader.startsWith("Bearer ")) {
+            UsernamePasswordAuthenticationToken auth = getAuthentication(request);
+            if (auth != null) SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        final String token = request.getHeader(AUTHORIZATION).substring(7);
+
+        jwtUtil.validateToken(token);
+
+        String username = jwtUtil.getUsername(token);
+        Claims claims = jwtUtil.getClaims(token);
+        List<GrantedAuthority> authorities = jwtUtil.getAuthorities(claims);
+
+        return username != null ? new UsernamePasswordAuthenticationToken(username, null, authorities) : null;
+    }
+}
