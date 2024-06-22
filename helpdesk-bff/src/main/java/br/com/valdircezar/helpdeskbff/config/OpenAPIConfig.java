5debefaaff9a60f64d13b2672bab662ab72dbf9c
valdircezar <valdir_jesus@sicredi.com.br>
2024-06-22 08:09:18 -0300

    feat: add swagger security config

--- helpdesk-bff/src/main/java/br/com/valdircezar/helpdeskbff/config/OpenAPIConfig.java
+++ helpdesk-bff/src/main/java/br/com/valdircezar/helpdeskbff/config/OpenAPIConfig.java
@@ -1,7 +1,10 @@
 package br.com.valdircezar.helpdeskbff.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +23,14 @@ public class OpenAPIConfig {
                         .title(title)
                         .description(description)
                         .version(version)
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
                 );
     }
 }
